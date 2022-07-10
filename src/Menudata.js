cdc4b62618c6be0f2708c5d0dbe55dfb7f1957df
Tej Patel <tejpatel317@gmail.com>
2022-07-10 18:14:36 -0400

    Adds Menudata

---
+++ src/Menudata.js
@@ -0,0 +1,68 @@
+const menuData = 
+[
+    {
+        "id" : 1,
+        "item" : "Chicken Tikka Masala",
+        "url" : "https://www.krumpli.co.uk/wp-content/uploads/2021/03/Chicken-Tikka-Masala-2-720x720.jpg",
+        "price" : 15.99,
+        "quantity" : 0
+    },
+    {
+        "id" : 2,
+        "item" : "Palak Paneer",
+        "url" : "https://www.cookwithmanali.com/wp-content/uploads/2019/08/Palak-Paneer-500x500.jpg",
+        "price" : 13.99,
+        "quantity" : 0
+    },
+    {
+        "id" : 3,
+        "item" : "Chole",
+        "url" : "https://lovelaughmirch.com/wp-content/uploads/2020/10/Punjabi-CHole-_7JPEGJPEG.jpg",
+        "price" : 12.99,
+        "quantity" : 0
+    },
+    {
+        "id" : 4,
+        "item" : "Dhai Puri",
+        "url" : "https://cdn3.foodviva.com/static-content/food-images/snacks-recipes/dahi-puri/dahi-puri.jpg",
+        "price" : 7.99,
+        "quantity" : 0
+    },
+    {
+        "id" : 5,
+        "item" : "Samosa Chaat",
+        "url" : "https://carameltintedlife.com/wp-content/uploads/2021/11/samosa-chaat-5-2-500x375.jpg",
+        "price" : 8.99,
+        "quantity" : 0
+    },
+    {
+        "id" : 6,
+        "item" : "Tandoori Chicken",
+        "url" : "https://dinnerthendessert.com/wp-content/uploads/2018/03/Easy-Tandoori-Chicken-1.jpg",
+        "price" : 10.99,
+        "quantity" : 0
+    },
+    {
+        "id" : 7,
+        "item" : "Pav Bhaji",
+        "url" : "https://images.lifestyleasia.com/wp-content/uploads/sites/7/2022/02/01171421/YFL-Pav-Bhaji-2.jpg",
+        "price" : 12.99,
+        "quantity" : 0
+    },
+    {
+        "id" : 8,
+        "item" : "Mango Lassi",
+        "url" : "https://www.cuisinart.com/globalassets/catalog/appliances/food-processors/complete-chef-cooking-food-processor/mangolassi_653475121.jpg",
+        "price" : 6.99,
+        "quantity" : 0
+    },
+    {
+        "id" : 9,
+        "item" : "Falooda",
+        "url" : "https://i0.wp.com/theindianclaypot.com/wp-content/uploads/2017/06/FALOODA-SMALLER.jpg?fit=1620%2C1080",
+        "price" : 7.99,
+        "quantity" : 0
+    }
+]
+
+export default menuData;

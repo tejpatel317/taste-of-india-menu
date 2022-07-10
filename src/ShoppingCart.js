7b644834d699f4aba17314c69fcd25092296f356
Tej Patel <tejpatel317@gmail.com>
2022-07-10 19:21:16 -0400

    Maps shoppingcart comp

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,7 +1,23 @@
 import React from "react";
 import {Container} from 'react-bootstrap'
+import Footer from "./Footer";
 
-function ShoppingCart() {
+function ShoppingCart({cartItems, numberOfItems, removeFromCart}) {
+
+    const shoppingCartComponents = cartItems.map((item) => {
+        return (
+            <div className="row border border-3 m-3 p-0 border-warning" key={item.id}>
+                <div className="col-md-3"><img className="rounded m-3" src={item.url} width="100"/></div>
+                <div className="col-md-4 my-auto h4">Palak Panner</div>
+                    <div className="col-md-2 my-auto h5">Quantity: {item.quantity}</div>
+                    <div className="col-md-1 my-auto h5">${item.price}</div>
+                    <div className="col-md-2 my-auto h5">
+                        <button className="btn btn-danger btn-lg py-2" onClick={() => removeFromCart(item.id)}>Remove</button>
+                    </div>
+            </div>
+        )
+    })
+
     return (
         <Container>
             <h1>Hello from ShoppingCart</h1>

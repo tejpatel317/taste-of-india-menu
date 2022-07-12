84623920e6b991d6ff761372b9a29e1716db01db
Tej Patel <tejpatel317@gmail.com>
2022-07-11 20:28:58 -0400

    Reviews details

--- src/Reviews.js
+++ src/Reviews.js
@@ -1,11 +1,16 @@
-import React from "react";
-import {Container} from 'react-bootstrap'
+import {React, useState} from "react";
+import Footer from "./Footer";
+import {Container, Row, Col, Card} from 'react-bootstrap';
 
 function Reviews() {
     return (
-        <Container>
-            <h1>Hello from Reviews</h1>
-        </Container>
+        <>
+            <Container fluid className="hm-auto bg-light">
+                <h1 className="text-center my-3 fontstyle">Customer Reviews</h1>
+                <h4 className="text-center w-50 mx-auto my-4 fontstyle">If you enjoyed your food and time at the restaurant or have feedback on how we can improve, we encourage you to submit a rating by filling out the form below.</h4>
+            </Container>
+            <Footer></Footer>
+        </>
     )
 }
 
--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -7,7 +7,7 @@ function ShoppingCart({cartItems, numberOfItems, removeFromCart}) {
     const shoppingCartComponents = cartItems.map((item) => {
         return (
             <div className="row border border-3 m-3 p-0 border-warning" key={item.id}>
-                <div className="col-md-3"><img className="rounded m-3" src={item.url} width="100"/></div>
+                <div className="col-md-3"><img className="rounded m-3" src={item.url} alt="Food Item" width="100"/></div>
                 <div className="col-md-4 my-auto h4">Palak Panner</div>
                     <div className="col-md-2 my-auto h5">Quantity: {item.quantity}</div>
                     <div className="col-md-1 my-auto h5">${item.price}</div>
